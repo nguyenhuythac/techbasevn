dd3eca21e8048e7d67ea6c68eefb81941aec4b80
dophy <nguyenhuythac@gmail.com>
2021-01-18 21:09:43 +0700

    jwt

---
+++ src/main/java/com/techbasevn/service/impl/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.techbasevn.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return new User("techbasevn","techbasevn",new ArrayList<>());
+	}
+	
+}
